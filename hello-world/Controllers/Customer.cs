d6cdb406536f7cb67464341fc674214aa0573172
Amith Nambiar <Amith Nambiar>
2018-07-05 10:10:56 +1000

    initial commit

---
+++ hello-world/Controllers/Customer.cs
@@ -0,0 +1,13 @@
+﻿
+namespace helloworld
+{
+    public class Customer
+    {
+        public string name { get; set; }
+
+        public Customer(string Cname)
+        {
+            name = Cname;
+        }
+    }
+}
---
+++ hello-world/Controllers/Hello.cs
@@ -0,0 +1,13 @@
+﻿
+namespace helloworld
+{
+    public class Hello
+    {
+        public string message { get; set; }
+
+        public Hello(string v)
+        {
+            this.message = v;
+        }
+    }
+}
